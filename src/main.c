9d81b61925cb7845cbefc3b8ab320eb6dc133ebd
Colin Pireyre <copireyr@student.hive.fi>
2024-10-04 14:12:42 +0300

    Include sysexits.h and errno.h for error codes

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 12:08:02 by copireyr          #+#    #+#             */
-/*   Updated: 2024/10/04 13:46:51 by copireyr         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:12:19 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <sysexits.h>
+#include <errno.h>
 #include <stdbool.h>
 #include "minishell.h"
 #include "tokenize.h"
@@ -34,23 +36,25 @@ char	*arena_readline(t_arena arena, const char *prompt)
 	return (result);
 }
 
+void	print_ast(t_ast_node *root, size_t level);
+
 int	main(int argc, char **argv, char **envp)
 {
-	t_list	**env;
-	t_token	*xs;
-	char	*user_input_line;
-	bool	should_exit_shell;
-	t_arena arena;
+	t_list		**env;
+	char		*user_input_line;
+	bool		should_exit_shell;
+	t_ast_node	*ast;
+	t_arena		arena;
 
 	if (argc > 1)
 	{
-		ft_dprintf(2, "%s: error: program does not accept any arguments\n", argv[0]);
-		return (64);
+		ft_dprintf(2, "Usage: %s", argv[0]);
+		return (EX_USAGE);
 	}
 	set_signal_handler();
 	env = init_env(envp);
 	if (!env)
-		exit(1);
+		return (ENOMEM);
 	should_exit_shell = false;
 	while (!should_exit_shell)
 	{
@@ -58,11 +62,11 @@ int	main(int argc, char **argv, char **envp)
 		if (!arena)
 			break ;
 		user_input_line = arena_readline(arena, MINISHELL_PROMPT);
-		should_exit_shell = !user_input_line || ft_streq(user_input_line, "exit");
+		should_exit_shell = !user_input_line;
 		if (!should_exit_shell && *user_input_line)
 		{
 			add_history(user_input_line);
-			t_ast_node *ast = parse(arena, user_input_line, *env);
+			ast = parse(arena, user_input_line, *env);
 			print_ast(ast, 0);
 		}
 		arena_dispose(&arena);
