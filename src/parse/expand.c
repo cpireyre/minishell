89e51b0b11b4bae29e0a0ab5d4044c89081d3353
Colin <cpireyre@student.42.fr>
2024-10-10 11:10:29 +0300

    Refactor expand_str to 20 lines

--- src/parse/expand.c
+++ src/parse/expand.c
@@ -6,7 +6,7 @@
 /*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 10:00:45 by copireyr          #+#    #+#             */
-/*   Updated: 2024/10/10 10:02:49 by copireyr         ###   ########.fr       */
+/*   Updated: 2024/10/10 11:10:23 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,79 +16,90 @@
 
 static char	*val(t_list *env, const char *key, size_t length_key);
 
-char	*expand_str(t_arena arena, t_list *env, const char *str)
-{
-    bool	expand_anyway = false;
-    char	*substrings[256];
-    int		count_substrings = 0;
-    t_arena	scratch = arena_new();
-    const char	*ptr;
+/*
+ * static variable is dangerous here, because the state will NOT
+ * be wiped clean when we parse the next command. However we know
+ * from the tokenizer that double quotes only occur in pairs, so
+ * expand_anyway should always be false when we are done expanding.
+*/
 
-    if (!scratch)
-	return (NULL);
-    while (*str)
-    {
-	ptr = str;
-	while (*str && *str != '$')
+int	find_next_expandable(const char *str)
+{
+	static bool	expand_anyway = false;
+	int			i;
+
+	i = 0;
+	while (str[i] && str[i] != '$')
 	{
-	    if (*str == '"')
-		expand_anyway = !expand_anyway;
-	    if (*str == '\'' && !expand_anyway)
-		str = ft_strchrnul(str + 1, '\'');
-	    str++;
+		if (str[i] == '"')
+			expand_anyway = !expand_anyway;
+		if (str[i] == '\'' && !expand_anyway)
+			while (str[++i] != '\'')
+				;
+		i++;
 	}
-	substrings[count_substrings++] = ft_arena_strndup(scratch, ptr, str - ptr);
-	if (!substrings[count_substrings - 1])
-	    return (NULL);
-	if (*str++ != '$')
-	    break ;
-	ptr = str;
-	while (ft_isalnum(*str) || *str == '_')
-	    str++;
-	char *expandable = ft_arena_strndup(scratch, ptr, str - ptr);
-	char *match = val(env, expandable, str - ptr);
-	if (match)
-	    substrings[count_substrings++] = match;
-    }
-    char *result = ft_arena_strjoin(arena, substrings, count_substrings);
-    arena_dispose(&scratch);
-    return (result);
+	return (i);
+}
+
+char	*expand_str(t_arena arena, t_list *env, const char *end)
+{
+	char		*substrings[256];
+	int			count;
+	const char	*start;
+
+	count = 0;
+	while (*end)
+	{
+		start = end;
+		end += find_next_expandable(end);
+		substrings[count++] = ft_arena_strndup(arena, start, end - start);
+		if (!substrings[count - 1])
+			return (NULL);
+		if (*end == '$')
+		{
+			start = ++end;
+			while (ft_isalnum(*end) || *end == '_')
+				end++;
+			substrings[count++] = val(env, start, end - start);
+		}
+	}
+	return (ft_arena_strjoin(arena, substrings, count));
 }
 
 void	expand(t_ast_node *ast, t_arena arena, t_list *env)
 {
-    size_t	i;
+	size_t	i;
 
-    if (!ast)
-	return ;
-    i = 0;
-    while (i < ast->n_children)
-    {
-	if (ast->children[i]->type == AST_WORD)
-	    ast->children[i]->token.value = expand_str(arena, env, ast->children[i]->token.value);
-	i++;
-    }
-    i = 0;
-    while (i < ast->n_children)
-    {
-	expand(ast->children[i], arena, env);
-	i++;
-    }
+	if (!ast)
+		return ;
+	i = 0;
+	while (i < ast->n_children)
+	{
+		if (ast->children[i]->type == AST_WORD)
+			ast->children[i]->token.value = expand_str(arena, env, ast->children[i]->token.value);
+		i++;
+	}
+	i = 0;
+	while (i < ast->n_children)
+	{
+		expand(ast->children[i], arena, env);
+		i++;
+	}
 }
 
 static char	*val(t_list *env, const char *key, size_t length_key)
 {
-    char	*curr;
+	char	*curr;
 
-    while (env)
-    {
-	size_t i = 0;
-	curr = env->content;
-	while (curr[i] != '=')
-	    i++;
-	if (i == length_key && !ft_memcmp(key, curr, length_key))
-	    return (curr + i + 1);
-	env = env->next;
-    }
-    return (NULL);
+	while (env && length_key)
+	{
+		size_t i = 0;
+		curr = env->content;
+		while (curr[i] != '=')
+			i++;
+		if (i == length_key && !ft_memcmp(key, curr, length_key))
+			return (curr + i + 1);
+		env = env->next;
+	}
+	return ("");
 }
