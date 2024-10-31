8ecccf5a4418b2ac378ca36db68b47b911d920d6
Colin Pireyre <copireyr@student.hive.fi>
2024-10-31 13:51:01 +0200

    Handle dequoting properly in parse step

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,36 +6,11 @@
 /*   By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:22:27 by pleander          #+#    #+#             */
-/*   Updated: 2024/10/30 14:19:35 by pleander         ###   ########.fr       */
+/*   Updated: 2024/10/31 13:50:32 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void	remove_outer_quotations(char *arg)
-{
-	char	*qs;
-	char	*qe;
-	int		i;
-
-	qs = NULL;
-	qe = NULL;
-	i = 0;
-	while (arg[i])
-	{
-		if (!qs && (arg[i] == '\"' || arg[i] == '\''))
-			qs = &arg[i];
-		else if (qs && arg[i] == *qs)
-			qe = &arg[i];
-		if (qs && qe)
-			break ;
-		i++;
-	}
-	if (!qe || !qs)
-		return ;
-	ft_memmove(qe, qe + 1, ft_strlen(qe + 1) + 1);
-	ft_memmove(qs, qs + 1, ft_strlen(qs + 1) + 1);
-}
 
 int	echo(char **args)
 {
@@ -53,10 +28,8 @@ int	echo(char **args)
 		}
 		while (args[i + 1])
 		{
-			remove_outer_quotations(args[i]);
 			ft_printf("%s ", args[i++]);
 		}
-		remove_outer_quotations(args[i]);
 		ft_printf("%s", args[i]);
 	}
 	if (nl)
