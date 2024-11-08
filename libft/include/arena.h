872c7d5e17fd78423a2be2a53da22ba23ffffe16
Colin <cpireyre@student.42.fr>
2024-11-08 11:35:12 +0200

    Simplify arena: remove alignment & memory blocks

--- libft/include/arena.h
+++ libft/include/arena.h
@@ -6,46 +6,26 @@
 /*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:04:53 by copireyr          #+#    #+#             */
-/*   Updated: 2024/05/28 11:14:58 by copireyr         ###   ########.fr       */
+/*   Updated: 2024/11/08 11:34:02 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARENA_H
 # define ARENA_H
 
-# include <stddef.h>
+#include <stddef.h>
 
-typedef struct s_arena	*t_arena;
-
-struct s_arena
+typedef struct s_arena_node
 {
-	t_arena	prev;
-	char	*avail;
-	char	*limit;
-};
+	void				*ptr;
+	struct s_arena_node	*next;
+}	t_arena_node;
 
-union u_align
-{
-	int			i;
-	long		l;
-	long		*lp;
-	void		*p;
-	void		(*fp)(void);
-	float		f;
-	double		d;
-	long double	ld;
-};
+typedef t_arena_node**	t_arena;
 
-union u_header
-{
-	struct s_arena	b;
-	union u_align	a;
-};
-
-extern t_arena	arena_new(void);
-extern void		arena_dispose(t_arena *ap);
-extern void		*arena_alloc(t_arena arena, size_t nbytes);
-extern void		*arena_calloc(t_arena arena, size_t count, size_t nbytes);
-extern void		arena_free(t_arena arena);
+t_arena	arena_new(void);
+void	arena_dispose(t_arena *ap);
+void	*arena_alloc(t_arena arena, size_t nbytes);
+void	*arena_calloc(t_arena arena, size_t count, size_t nbytes);
 
 #endif /* ARENA_H */
