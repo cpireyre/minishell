3a2dbe178784c9a77a354687c5666a402d153442
Colin <cpireyre@student.42.fr>
2024-11-19 16:32:56 +0200

    Include signal.h

--- include/signals.h
+++ include/signals.h
@@ -6,13 +6,14 @@
 /*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 12:46:57 by copireyr          #+#    #+#             */
-/*   Updated: 2024/11/19 16:28:41 by copireyr         ###   ########.fr       */
+/*   Updated: 2024/11/19 16:32:39 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNALS_H
 # define SIGNALS_H
 
+# include <signal.h>
 int		*exit_code(void);
 int		set_signal_handler(void);
 void	set_signal_handlers(void (*sigquit_fn)(int), void (*sigint_fn)(int));
